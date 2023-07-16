171c3ebd2f760492844f337c755beb801092d6c0
nfsoft <nasir24k@gmail.com>
2023-07-16 18:30:30 +0600

    complete resturaunt website front end design
    i have to create menu section, reservation section, Testimonial section, blog section and footer section.

--- src/js/testimonial.js
+++ src/js/testimonial.js
@@ -1,37 +1,22 @@
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-      loop: true,
-      margin: 30,
-      nav: true,
-      dots: false,
-      navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-      responsive: {
-        0: {
-          items: 1
-        },
-        768: {
-          items: 1
-        },
-        992: {
-          items: 1
-        }
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
       }
-    });
+    }
   });
-  $(document).ready(function() {
-    $('.counter').each(function() {
-      var $this = $(this);
-      var target = parseInt($this.attr('data-target'));
-      var count = 0;
-  
-      $({ count: 0 }).animate({ count: target }, {
-        duration: 3000,
-        easing: 'linear',
-        step: function() {
-          var roundedCount = Math.ceil(this.count);
-          $this.text(roundedCount);
-        }
-      });
-    });
-  });
-  
+});
